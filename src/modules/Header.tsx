df3562298248ec250862f656024732d7c5cc1ecc
anton <komogorcevanton10@gmail.com>
2026-08-30 19:25:13 +0800

    feat: added adaptive for Header

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -5,6 +5,8 @@ import logo from "/assets/favicon/icon.svg";
 import busket from "/assets/sections/header/basket.svg";
 import busketHover from "/assets/sections/header/basket-hover.svg";
 import busketActive from "/assets/sections/header/basket-active.svg";
+import burger from "/assets/sections/header/burger.svg";
+import burgerActive from "/assets/sections/header/burger-active.svg";
 
 import "./styles/header.css";
 
@@ -12,6 +14,10 @@ const header = ["Репродукции", "Новинки", "О нас"];
 
 export const Header = () => {
   const [stateBusket, setStateBusket] = useState("passive");
+  const [isBurgerActive, setIsBurgerActive] = useState(false);
+
+  const burgerMenu = (styles: object | undefined) =>
+    isBurgerActive ? styles : undefined;
 
   return (
     <header>
@@ -20,12 +26,33 @@ export const Header = () => {
         <h1 className="icon">Ink. House</h1>
       </a>
 
-      <nav>
-        <section className="nav-buttons">
+      <nav
+        style={burgerMenu({
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: 30,
+        })}
+      >
+        <img
+          onClick={() => setIsBurgerActive(!isBurgerActive)}
+          src={isBurgerActive ? burgerActive : burger}
+          alt=""
+        />
+
+        <section
+          style={burgerMenu({
+            display: "flex",
+            fontSize: 18,
+            gap: 40,
+            flexDirection: "column",
+          })}
+          className="nav-buttons"
+        >
           {header.map((header) => (
             <Link to="/">{header}</Link>
           ))}
         </section>
+
         <Link
           onMouseEnter={() => setStateBusket("active")}
           onMouseLeave={() => setStateBusket("passive")}
@@ -34,6 +61,7 @@ export const Header = () => {
           to="/"
         >
           <img
+            style={isBurgerActive ? { display: "none" } : undefined}
             src={
               stateBusket == "passive"
                 ? busket
